0918e1315343ca533bea2538c33c56b65e356b67
Mudi Ugbowanko <mudi@renegare.com>
2015-12-22 11:13:41 +0000

    ok we have a minimum compliant adapter

--- test/lib/adapter/webdriverio.spec.js
+++ test/lib/adapter/webdriverio.spec.js
@@ -2,6 +2,7 @@ var test = require('ava')
 var sinon = require('sinon')
 var src = require('sequire')
 var proxy = require('proxyquire').noCallThru()
+var Element = src('index').Element
 
 test.beforeEach('setup stubs', t => {
   var driver = {}
@@ -22,40 +23,8 @@ test.beforeEach('setup stubs', t => {
   t.context.adapter = adapter
 })
 
-test('get driver', t => {
-  var adapter = t.context.adapter
-  var wdio = t.context.wdio
-  var driver = t.context.driver
-  var wdOpts = t.context.wdOpts
-
-  t.same(driver, adapter.driver)
-  t.ok(driver.init.calledOnce)
-  t.ok(wdio.remote.calledOnce)
-  t.same(wdio.remote.lastCall.args, [wdOpts])
-})
-
-test('open url', t => {
-  var adapter = t.context.adapter
-  var driver = t.context.driver
-
-  driver.url = sinon.stub().returns(Promise.resolve())
-
-  return adapter.open('http://localhost/some/where')
-    .then(() => {
-      t.ok(driver.url.calledOnce)
-      t.same(driver.url.lastCall.args, ['http://localhost/some/where'])
-    })
-})
-
-test('get current browser url', t => {
-  var adapter = t.context.adapter
-  var driver = t.context.driver
-
-  driver.getUrl = sinon.stub().returns(Promise.resolve('http://localhost/some/where/else'))
-
-  return adapter.url.then(url => {
-    t.same(url, 'http://localhost/some/where/else')
-  })
+test('get exposable methods', t => {
+  t.same(t.context.adapter.methods, ['find', 'findAll'])
 })
 
 test('find single element matching selector ', t => {
@@ -66,7 +35,7 @@ test('find single element matching selector ', t => {
   driver.element = sinon.stub().returns(Promise.resolve())
   driver.waitUntil.returns(Promise.resolve(element))
 
-  return adapter.find('.element')
+  return adapter.find(new Element(undefined, '.element'))
     .then(e => {
       t.same(driver.element.lastCall.args, ['.element'])
       t.same(e, element)
@@ -81,39 +50,39 @@ test('findAll element matching selector', t => {
   driver.elements = sinon.stub().returns(Promise.resolve())
   driver.waitUntil.returns(Promise.resolve(elements))
 
-  return adapter.findAll('.elements')
+  return adapter.findAll(new Element(undefined, '.elements'))
     .then(e => {
       t.same(driver.elements.lastCall.args, ['.elements'])
       t.same(e, elements)
     })
 })
 
-test('clone and contextulise adapter (findAll)', t => {
-  var adapter = t.context.adapter.contextulise('.root')
-  var driver = t.context.driver
-  var elements = [{some: 'element'}, {some: 'element'}, {some: 'element'}]
-
-  driver.elements = sinon.stub().returns(Promise.resolve())
-  driver.waitUntil.returns(Promise.resolve(elements))
-
-  return adapter.findAll('.elements')
-    .then(e => {
-      t.same(driver.elements.lastCall.args, ['.root .elements'])
-      t.same(e, elements)
-    })
-})
-
 test('clone and contextulise adapter (find)', t => {
-  var adapter = t.context.adapter.contextulise('.root')
+  var adapter = t.context.adapter.contextulise(new Element(null, '.root'))
   var driver = t.context.driver
   var element = {some: 'element'}
 
   driver.element = sinon.stub().returns(Promise.resolve())
   driver.waitUntil.returns(Promise.resolve(element))
 
-  return adapter.find('.element')
+  return adapter.find(new Element(null, '.element'))
     .then(e => {
       t.same(driver.element.lastCall.args, ['.root .element'])
       t.same(e, element)
+    })
+})
+
+test('clone and contextulise adapter (findAll)', t => {
+  var adapter = t.context.adapter.contextulise(new Element(null, '.root'))
+  var driver = t.context.driver
+  var elements = [{some: 'element'}, {some: 'element'}, {some: 'element'}]
+
+  driver.elements = sinon.stub().returns(Promise.resolve())
+  driver.waitUntil.returns(Promise.resolve(elements))
+
+  return adapter.findAll(new Element(null, '.elements'))
+    .then(e => {
+      t.same(driver.elements.lastCall.args, ['.root .elements'])
+      t.same(e, elements)
     })
 })
