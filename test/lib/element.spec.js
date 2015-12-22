10330a61433ae7bde603605322377f5be3f167cb
Mudi Ugbowanko <mudi@renegare.com>
2015-12-22 09:19:20 +0000

    element now respects 3rd index param and passes it on to adapter methods

--- test/lib/element.spec.js
+++ test/lib/element.spec.js
@@ -7,6 +7,11 @@ class MockAdapter {
   constructor() {
     this.findStub = sinon.stub()
     this.findAllStub = sinon.stub()
+    this.methodsStub = sinon.stub()
+  }
+
+  get methods () {
+    return this.methodsStub.apply(null, arguments)
   }
 
   find () {
@@ -22,20 +27,26 @@ test('Element exposes proxy adapter methods', async t => {
   var element = {some: 'object'}
   var adapter = new MockAdapter()
   var findStub = adapter.findStub.returns(Promise.resolve(element))
+  var methodsStub = adapter.methodsStub.returns(['find'])
 
   var e = new Element(adapter, 'button.red')
+  t.same(e.index, undefined)
+  t.same(e.findAll, undefined)
   t.same(await e.find(), element)
   t.ok(findStub.calledOnce)
-  t.same(findStub.lastCall.args, ['button.red'])
+  t.same(findStub.lastCall.args, [undefined, 'button.red'])
 })
 
 test('Element correctly passes on additional arguments', async t => {
   var elements = [{some: 'object'}, {some: 'object'}, {some: 'object'}]
   var adapter = new MockAdapter()
   var findAllStub = adapter.findAllStub.returns(Promise.resolve(elements))
+  var methodsStub = adapter.methodsStub.returns(['findAll'])
 
-  var e = new Element(adapter, 'button.red')
+  var e = new Element(adapter, 'button.red', 1)
+  t.same(e.index, 1)
+  t.same(e.find, undefined)
   t.same(await e.findAll('additional', 'args'), elements)
   t.ok(findAllStub.calledOnce)
-  t.same(findAllStub.lastCall.args, ['button.red', 'additional', 'args'])
+  t.same(findAllStub.lastCall.args, [1, 'button.red', 'additional', 'args'])
 })
